72b2cc05d19490d9d81f6d68d0e26b02f8e413f6
George Kalpaktsoglou <gkalpak@fogus.gr>
2022-12-25 13:34:35 +0200

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using static Apress.Sample.gRPC.v1.CountryService;
+
+var loggerFactory = LoggerFactory.Create(logging =>
+{
+
+    logging.AddConsole();
+
+    logging.SetMinimumLevel(LogLevel.Trace);
+});
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001", new
+GrpcChannelOptions
+{
+    LoggerFactory = loggerFactory
+
+});
+var countryClient = new CountryServiceClient(channel);
